e08bd006bbe4031bb296961879c1c7eb1cc9e509
Luca <40441626+Cipulot@users.noreply.github.com>
2020-12-27 11:28:20 +0100

    Day 25 solution
    🎄Merry Christmas and Happy New Year🎄

---
+++ day25.py
@@ -0,0 +1,28 @@
+def solution(data):
+    a = data[0]
+    b = data[1]
+    i=1
+    j=1
+    cnt_1=0
+    cnt_2=0
+    while(i!=a):
+        i*=7
+        i%=20201227
+        cnt_1+=1
+
+    while(j!=b):
+        j*=7
+        j%=20201227
+        cnt_2+=1
+
+    ans=1
+    for i in range(0,cnt_2):
+        ans*=a
+        ans%=20201227
+    return ans
+
+if __name__ == '__main__':
+    with open('25.txt') as _file:
+        data = [int(line) for line in _file.read().splitlines()]
+
+    print("Final solution: ", solution(data))
